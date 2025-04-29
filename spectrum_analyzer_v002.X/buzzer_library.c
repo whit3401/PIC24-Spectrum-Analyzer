76b1b2f577af9205e8963fe8191631228bb7716c
tysenww <158533737+tysenww@users.noreply.github.com>
2025-04-29 17:46:43 -0500

    changed buzzer pwm timer from timer 3 to timer 2 to avoid overlap with ADC

--- spectrum_analyzer_v002.X/buzzer_library.c
+++ spectrum_analyzer_v002.X/buzzer_library.c
@@ -20,20 +20,20 @@ void buzzer_pwm_setup(void){
     
     TRISBbits.TRISB4 = 0; // sets RB4 as output mode
     
-    // Configure Timer 3 for 20ms period
-    T3CON = 0;  //Stop and reset timer configuration
-    T3CONbits.TCKPS = 0b01; //sets prescaler to 1:8
-    TMR3 = 0;        // initialize to 0: Timer 3
-    PR3 = 999; // 2kHz PWM frequency (TO CHANGE VALUE, NOT NUMERICALLY ACCURATE)
-    T3CONbits.TON = 0;
+    // Configure Timer 2
+    T2CON = 0;  //Stop and reset timer configuration
+    T2CONbits.TCKPS = 0b01; //sets prescaler to 1:8
+    TMR2 = 0;        // initialize to 0: Timer 3
+    PR2 = 999; // random initial period
+    T2CONbits.TON = 0;
     
     // set OC1 for servo PWM (1-2ms pulse in 20ms frame)
     
-    OC1R = PR3/2; //Set 50% Duty Cycle
-    OC1RS = PR3/2; // Update shadow register as well
+    OC1R = PR2/2; //Set 50% Duty Cycle
+    OC1RS = PR2/2; // Update shadow register as well
     OC1CON = 0;
     OC1CONbits.OCM = 0b110; // PWM mode, fault pin disabled
-    OC1CONbits.OCTSEL = 1; // set Timer 3 as clock source
+    OC1CONbits.OCTSEL = 0; // set Timer 2 as clock source
 }
 
 
