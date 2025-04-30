00aab2415de22b8f489dc9190f70d3d93e2e2b0d
John Gibbens <johngibbens04@gmail.com>
2025-04-29 19:01:07 -0500

    Added timer1 to turn the adc off after 0.25s after the button is pressed

--- spectrum_analyzer_v002.X/ADC_microphone_library.c
+++ spectrum_analyzer_v002.X/ADC_microphone_library.c
@@ -5,12 +5,14 @@
  * Created on April 15, 2025, 9:59 PM
  */
 
+#include <p24FJ64GA002.h>
+
 #include "xc.h"
 #include "ADC_microphone_library.h"
 
-#define BUFFER_SIZE 500 // buffer size changed from 2048 to 500 to accoodate PIC24's memory space
+#define ARRAY_SIZE 800 // array size changed from 2048 to 800 to accoodate PIC24's memory space
 #define SAMPLE_INTERVAL 1 //the time between each sample, currently placeholder value
-volatile float buffer[BUFFER_SIZE][2];
+volatile float adcVals[ARRAY_SIZE][2];
 volatile int sampleCount = 0; 
 volatile int sampleReady = 0;
 
@@ -18,28 +20,31 @@ volatile int sampleReady = 0;
 void begin_sampling(){
     //enable ADC interrupts
     sampleReady = 0;
+    init_arr();
     AD1CON1bits.ADON = 1;    // Turn on ADC
     _AD1IE = 1;
-    init_buffer();
+    T1CONbits.TON = 1;
+    TMR1 = 0;
 }
 
 void end_sampling(){
     //disable ADC interrupts
     AD1CON1bits.ADON = 0;    // Turn off ADC
     _AD1IE = 0;
+    T1CONbits.TON = 0; //Turns off timer when sampling is off
     sampleReady = 1;
 }
 /*
  * This array will return values between 0 and 1023, to convert to voltage, 
  * multiply by 3.3/1024 
  */
-    //ERROR:  !! THIS NEEDS TO BE CHANGED TO ACCOMODATE NEW BUFFER SIZE ^^ !!
+    //ERROR:  !! THIS NEEDS TO BE CHANGED TO ACCOMODATE NEW ARRAY SIZE ^^ !!
 
 volatile float* get_digital_signal_data(){
     //return array of data samples ordered chronologically
 
     sampleReady = 0;
-    return buffer;
+    return adcVals;
 }
 
 int is_sample_ready(){
@@ -68,10 +73,27 @@ void adc_init(void){
     _AD1IF = 0;
     _AD1IE = 0;
     
-    T3CON = 0x0001;
-    PR3 = 9999; 
+    T3CON = 0x0000;
+    T3CONbits.TCKPS = 0b00;
+    PR3 = 4999; //0.25 seconds sample time, 3200 samples per second (3.2KHz)
     TMR3 = 0;
     T3CONbits.TON = 1;
+}
+
+void timer1_init(void){
+    T1CON = 0x0000;
+    T1CONbits.TCKPS = 0b10;
+    TMR1 = 0;
+    PR1 = 62499;
+    _T1IF = 0;
+    _T1IE = 1;
+    T1CONbits.TON = 0;
+}
+
+void __attribute__((__interrupt__, auto_psv)) _T1Interrupt(void){
+    //Interrupt for ADC
+    IFS0bits.T1IF = 0; 
+    end_sampling();
 }
 
 void __attribute__((__interrupt__, auto_psv)) _ADC1Interrupt(void){
@@ -81,19 +103,19 @@ void __attribute__((__interrupt__, auto_psv)) _ADC1Interrupt(void){
 }
 
 void put_val(float newValue){
-    buffer[sampleCount++][0] = newValue;
+    adcVals[sampleCount++][0] = newValue;
 }
 
-//sets all buffer values to zero, and resets sampleCount
-void init_buffer(){
-    for(int i = 0; i < BUFFER_SIZE; i++){
-        buffer[i][0] = 0;
-        buffer[i][1] = 0;
+//sets all array values to zero, and resets sampleCount
+void init_arr(){
+    for(int i = 0; i < ARRAY_SIZE; i++){
+        adcVals[i][0] = 0;
+        adcVals[i][1] = 0;
     }
     sampleCount = 0;
 }
 
 void microphone_setup(){
     adc_init();
-    init_buffer();
+    init_arr();
 }
